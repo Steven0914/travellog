620ab43894450d3b1217a253d190efdb467dfd3e
Steven0914 <shu914@naver.com>
2023-11-16 13:56:24 +0000

    32/비번찾기 페이지 수정 완료

--- frontend/src/components/Account/FindPwdForm.jsx
+++ frontend/src/components/Account/FindPwdForm.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import image2 from "../../assets/image/accountImage.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./AccountForm.css";
+import axios from 'axios';
 
 import { useState } from "react";
 
 const FindPwdForm = () => {
   const [inputUsername, setInputUsername] = useState("");
   const [inputEmail, setInputEmail] = useState("");
+  const [inputBirthday, setBirthday] = useState("");
 
   function usernameChangeHandler(event) {
     setInputUsername(event.target.value);
@@ -17,13 +19,39 @@ const FindPwdForm = () => {
     setInputEmail(event.target.value);
   }
 
+  function birthdayChangeHandler(event) {
+    setBirthday(event.target.value);
+  }
+
+  let navigate = useNavigate();
+  const params = new URLSearchParams();
+
   function submitHandler(event) {
     event.preventDefault();
     const pwdData = {
-      username: inputUsername,
+      name: inputUsername,
       email: inputEmail,
+      birthday: inputBirthday,
     };
     console.log(pwdData);
+    Object.keys(pwdData).forEach(key => params.append(key, pwdData[key]));
+    axios.post("https://api.travellog.site:8080/user/reset", pwdData,{
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Accept': '*/*'
+      }
+    })
+        .then((response) => {
+          // 상태 코드가 200이 아닌 경우에도 성공 메시지를 표시하고 페이지를 이동시킵니다.
+          alert("비밀번호 찾기에 성공했습니다.");
+          navigate("/");
+        })
+        .catch((error) => {
+            // 오류가 발생한 경우 오류 메시지를 표시합니다.
+            alert("비밀번호 찾기에 실패했습니다.");
+            }
+        );
+
   }
 
   return (
@@ -39,9 +67,11 @@ const FindPwdForm = () => {
         </Link>
         <form onSubmit={submitHandler}>
           <p>Username</p>
-          <input type="text" placeholder="Enter your User name" required onChange={usernameChangeHandler}></input>
+          <input type="text" name="name" placeholder="Enter your User name" required onChange={usernameChangeHandler}></input>
           <p>Email</p>
-          <input type="text" placeholder="Enter your email address" required onChange={emailChangeHandler}></input>
+          <input type="email" name="email" placeholder="Enter your email address" required onChange={emailChangeHandler}></input>
+          <p>Birth Day</p>
+          <input type="date" name="birthday" required onChange={birthdayChangeHandler} />
           <br />
           <button className="button">비밀번호 찾기</button>
         </form>
