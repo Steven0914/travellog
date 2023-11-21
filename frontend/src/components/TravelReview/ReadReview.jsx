bb6cb479404ed55f71367ea0795c86e92254aaef
Steven0914 <shu914@naver.com>
2023-11-21 15:40:01 +0000

    54/리뷰페이지와 상세페이지 구현

--- frontend/src/components/TravelReview/ReadReview.jsx
+++ frontend/src/components/TravelReview/ReadReview.jsx
@@ -1,13 +1,52 @@
-import React, {useEffect} from "react";
+import React, { useState, useEffect } from "react";
+import styles from "./ReadReview.module.css";
 import Navbar from "../UI/Navbar";
+import axios from "axios";
+import { Link, useLocation } from "react-router-dom";
+import arrowBack from "../../assets/arrow_back.svg";
 
 const ReadReview = () => {
-    return (
-        <>
-            <Navbar />
-            <div>리뷰 읽는 곳</div>
-        </>
-    );
+  const location = useLocation();
+  const reviewId = location.state.reviewId;
+  const [review, setReview] = useState([]);
+
+  useEffect(() => {
+    console.log(reviewId);
+    axios
+      .get(`https://api.travellog.site:8080/review/${reviewId}`, {})
+      .then((response) => {
+        setReview(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+    console.log(review.content);
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className={styles.body}>
+        <div className={styles.container}>
+          <div className={styles.header_box}>
+            <Link to="/ReviewList">
+              <img src={arrowBack} />
+            </Link>
+            <h1 className={styles.header}>Review</h1>
+            <img className={styles.hidden_arrow} src={arrowBack} />
+          </div>
+          <h1 className={styles.title}>{review.title}</h1>
+          <div className={styles.review_info}>
+            <p className={styles.locate}>{review.locate} 여행</p>
+            <p>{review.userName}</p>
+          </div>
+          <img className={styles.image} src={`${review.imgUrl}`} />
+          <p className={styles.content}>{review.content}</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ReadReview;
--- frontend/src/components/TravelReview/ReviewList.jsx
+++ frontend/src/components/TravelReview/ReviewList.jsx
@@ -1,15 +1,51 @@
-import React, {useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../UI/Navbar";
-import {Link} from "react-router-dom";
+import styles from "./ReviewList.module.css";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 const ReviewList = () => {
-    return (
-        <>
-            <Navbar />
-            <div>리뷰 리스트 보는 곳</div>
-            <Link to="/ReadReview" style={{ textDecoration: "none"}}><li className="nav-item">Read Review</li></Link>
-        </>
-    );
+  const [reviewlist, setReviewList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://api.travellog.site:8080/review", {})
+      .then((response) => {
+        setReviewList(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("There was an error!", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <Navbar />
+      <div className={styles.body}>
+        <p className={styles.title}>Read Review</p>
+        <div className={styles.container}>
+          {[...reviewlist].reverse().map((review) => (
+            <Link
+              to={`/ReadReview`}
+              state={{ reviewId: review.reviewId }}
+              key={review.reviewId}
+              style={{ textDecoration: "none" }}
+            >
+              <div className={styles.review}>
+                <img src={`${review.imgUrl}`} className={styles.image} />
+                <p className={styles.review_title}>{review.title}</p>
+                <div className={styles.place_writer}>
+                  <p>{review.locate}</p>
+                  <p>{review.userName}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ReviewList;
