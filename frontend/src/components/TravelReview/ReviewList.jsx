e2f7d8bb8202d6a50bc645764de5c66a839712bd
Steven0914 <shu914@naver.com>
2023-12-04 15:47:22 +0000

    리뷰리스트 조회기능, 반응형으로(#68)

--- frontend/src/components/TravelReview/ReviewList.jsx
+++ frontend/src/components/TravelReview/ReviewList.jsx
@@ -1,51 +1,56 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect} from "react";
 import Navbar from "../UI/Navbar";
 import styles from "./ReviewList.module.css";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import axios from "axios";
 
 const ReviewList = () => {
-  const [reviewlist, setReviewList] = useState([]);
+    const [reviewlist, setReviewList] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get("https://api.travellog.site:8080/review", {})
-      .then((response) => {
-        setReviewList(response.data);
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error("There was an error!", error);
-      });
-  }, []);
+    useEffect(() => {
+        axios
+            .get("https://api.travellog.site:8080/review", {})
+            .then((response) => {
+                setReviewList(response.data);
+                console.log(response.data);
+            })
+            .catch((error) => {
+                console.error("There was an error!", error);
+            });
+    }, []);
 
-  return (
-    <>
-      <Navbar />
-      <div className={styles.body}>
-        <p className={styles.title}>Read Review</p>
-        <div className={styles.container}>
-          {[...reviewlist].reverse().map((review) => (
-            <Link
-              to={`/ReadReview/${review.reviewId}`}
-              state={{ reviewId: review.reviewId }}
-              key={review.reviewId}
-              style={{ textDecoration: "none" }}
-            >
-              <div className={styles.review}>
-                <img src={`${review.imgUrl}`} className={styles.image} />
-                <p className={styles.review_title}>{review.title}</p>
-                <div className={styles.place_writer}>
-                  <p>{review.locate}</p>
-                  <p>{review.userName}</p>
+    return (
+        <>
+            <Navbar/>
+            <div className={styles.body}>
+                <p className={styles.title}>Read Review</p>
+                <div className={styles.container}>
+                    {[...reviewlist].reverse().map((review) => (
+                        <Link
+                            to={`/ReadReview/${review.reviewId}`}
+                            state={{reviewId: review.reviewId}}
+                            key={review.reviewId}
+                            style={{textDecoration: "none"}}
+                        >
+                            <div className={styles.review_box}>
+                                <div className={styles.review}>
+                                    <img src={`${review.imgUrl}`} className={styles.image} />
+                                    <div className={styles.title_view_container}>
+                                        <p className={styles.review_title}>{review.title}</p>
+                                        <p className={styles.view_count}>Views: {review.view}</p>
+                                    </div>
+                                    <div className={styles.place_writer}>
+                                        <p>{review.locate}</p>
+                                        <p>{review.userName}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </Link>
+                    ))}
                 </div>
-              </div>
-            </Link>
-          ))}
-        </div>
-      </div>
-    </>
-  );
+            </div>
+        </>
+    );
 };
 
 export default ReviewList;
