8995638555cdbebd6889b1e10208a6f30b2acbab
Steven0914 <shu914@naver.com>
2023-12-05 09:27:17 +0000

    댓글 기능 구현

--- frontend/src/components/TravelReview/ReadReview.jsx
+++ frontend/src/components/TravelReview/ReadReview.jsx
@@ -9,6 +9,8 @@ const ReadReview = () => {
   const location = useLocation();
   const reviewId = location.state.reviewId;
   const [review, setReview] = useState([]);
+  const [comment, setComment] = useState([]);
+  const [newComment, setNewComment] = useState("");
 
   useEffect(() => {
     console.log(reviewId);
@@ -21,8 +23,45 @@ const ReadReview = () => {
       .catch((error) => {
         console.error("There was an error!", error);
       });
-    console.log(review.content);
+
+    axios
+        .get(`https://api.travellog.site:8080/comment/${reviewId}`, {})
+        .then((response) => {
+          setComment(response.data);
+          console.log(response.data);
+        })
+        .catch((error) => {
+          console.error("There was an error!", error);
+        });
+    console.log(comment.content);
   }, []);
+
+  const handleCommentSubmit = (e) => {
+    e.preventDefault();
+    const token = localStorage.getItem("token");
+    axios
+        .post(`https://api.travellog.site:8080/comment/${reviewId}`, null, {
+          params: { content: newComment },
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then(() => {
+          setNewComment(""); // 댓글 입력란 초기화
+          // 댓글 추가 후 댓글 리스트 다시 불러오기
+          axios
+              .get(`https://api.travellog.site:8080/comment/${reviewId}`)
+              .then((response) => {
+                setComment(response.data);
+              })
+              .catch((error) => {
+                console.error("There was an error!", error);
+              });
+        })
+        .catch((error) => {
+          console.error("There was an error!", error);
+        });
+  };
+
+
 
   return (
     <>
@@ -43,6 +82,27 @@ const ReadReview = () => {
           </div>
           <img className={styles.image} src={`${review.imgUrl}`} />
            <p className={styles.content} dangerouslySetInnerHTML={{ __html: review.content }}></p>
+          <div className={styles.comments}>
+            <h2 className={styles.commentsTitle}>댓글</h2>
+            <form onSubmit={handleCommentSubmit} className={styles.newCommentForm}>
+              <input
+                  type="text"
+                  value={newComment}
+                  onChange={e => setNewComment(e.target.value)}
+                  placeholder="댓글을 입력하세요..."
+                  className={styles.newCommentInput}
+              />
+              <button type="submit" className={styles.newCommentButton}>댓글 작성</button>
+            </form>
+
+            {comment.map((item) => (
+                <div key={item.commentId} className={styles.comment}>
+                  <p className={styles.commentContent}>{item.content}</p>
+                  <p className={styles.commentDate}>{item.createdAt}</p>
+                </div>
+            ))}
+          </div>
+
         </div>
       </div>
     </>
