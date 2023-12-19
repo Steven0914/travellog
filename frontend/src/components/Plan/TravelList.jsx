b8e22cfceb1e9391a9807ef389ac973c53d92377
Steven0914 <shu914@naver.com>
2023-12-19 07:26:03 +0000

    96/Plan 페이지 UI 수정

--- frontend/src/components/Plan/TravelList.jsx
+++ frontend/src/components/Plan/TravelList.jsx
@@ -5,6 +5,7 @@ import noResult from "../../assets/image/noResult2.png";
 import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const TravelList = ({ selectedDay, locationList, setLocationList }) => {
+  console.log(locationList);
   const removePlaceHandler = (indexToRemove) => {
     setLocationList((prevState) => {
       let count = 0; // 선택된 날짜의 장소들 중에서 몇 번째인지를 세는 카운터
@@ -51,23 +52,36 @@ const TravelList = ({ selectedDay, locationList, setLocationList }) => {
 
   return (
     <>
+      <div>장소 초기화</div>
       <ul className={styles.travelList}>
         {locationList.length > 0 ? (
             locationList
               .filter((item) => item.day === selectedDay)
               .map((item, index) => (
                   <li className={styles.travelLocations} key={index}>
-                    <img
-                      className={styles.listIcon}
-                      src={icon2}
-                      onClick={() => moveDownHandler(index)}
-                    ></img>
-                    <div className={styles.locationName}>{item.name}</div>
-                    <img
-                      className={styles.deleteIcon}
-                      src={deleteIcon}
-                      onClick={() => removePlaceHandler(index)}
-                    ></img>
+                    <div className={styles.locationBoxSection}>
+                      <div className={styles.numberSection}>
+                        
+                      </div>
+                      <div className={styles.locationTextSection}>
+                        <div className={styles.locationName}>{item.name}</div>
+                        <div>{item.category2}</div>
+                      </div>
+                      <div className={styles.changeSeqBtn}>
+                        <img
+                            className={styles.listIcon}
+                            src={icon2}
+                            onClick={() => moveDownHandler(index)}
+                          ></img>
+                      </div>
+                    </div>
+                    <div className={styles.delBtnSection}>
+                      <button
+                        className={styles.deleteIcon}
+                        onClick={() => removePlaceHandler(index)}
+                      >X</button>
+                    </div>
+
                   </li>
               ))
         ) : (
