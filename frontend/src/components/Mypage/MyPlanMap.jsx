49bba9d33cf971f0c6000f95c5b619acd32dff93
Steven0914 <shu914@naver.com>
2023-12-19 13:21:18 +0000

    92/MyPlan 페이지 마커번호 및 점선 연결 구현

--- frontend/src/components/Mypage/MyPlanMap.jsx
+++ frontend/src/components/Mypage/MyPlanMap.jsx
@@ -5,13 +5,20 @@ const MyPlanMap = ({ planId, selectedDay }) => {
   const [planDetails, setPlanDetails] = useState([]);
   const mapRef = useRef(null); // useRef 사용
   const markersRef = useRef([]); // 마커를 저장할 useRef 생성
+  const linesRef = useRef(null);
 
   useEffect(() => {
     markersRef.current.forEach(({ marker, overlay }) => {
       marker.setMap(null);
       overlay.setMap(null);
     });
+
+    if (linesRef.current) {
+      linesRef.current.setMap(null); // 선을 지우는 부분
+    }
+
     markersRef.current = [];
+    const positions = []; // 마커의 위치를 저장할 배열
 
     axios
       .get(`https://api.travellog.site:8080/viewplan/${planId}`, {})
@@ -48,13 +55,14 @@ const MyPlanMap = ({ planId, selectedDay }) => {
 
           const overlay = new kakao.maps.CustomOverlay({
             position: locPosition,
-            content: `<div style="background-color: white; border: 1px solid #333; width: 20px; height: 20px; text-align: center; line-height: 20px; border-radius: 10px;">${seq}</div>`,
-            yAnchor: 1,
+            content: `<div style="background-color: white; border: 0px solid #333; width: 15px; height: 15px;  text-align: center; line-height: 13.5px; border-radius: 10px; font-size: 12px; font-weight: bold;">${seq}</div>`,
+            yAnchor: 2.2,
           });
 
           overlay.setMap(mapRef.current); // 사용자 정의 오버레이를 지도에 표시
 
           markersRef.current.push({ marker, overlay }); // 마커와 오버레이를 함께 markersRef에 추가
+          positions.push(locPosition); // 마커의 위치를 배열에 추가
         };
 
         markersRef.current.forEach(({ marker, overlay }) => {
@@ -67,6 +75,21 @@ const MyPlanMap = ({ planId, selectedDay }) => {
           const locPosition = new kakao.maps.LatLng(place.lat, place.lng);
           displayMarker(locPosition, place.name, place.seq);
         });
+        if(markersRef.current.length > 1) {
+          // 모든 마커가 생성된 후에 선을 그립니다.
+          linesRef.current = new kakao.maps.Polyline({
+            map: mapRef.current,
+            path: positions, // 마커의 위치를 순서대로 연결
+            strokeWeight: 2, // 선의 두께
+            strokeColor: "#FF0000", // 선의 색상
+            strokeOpacity: 1, // 선의 투명도
+            strokeStyle: 'dashed' // 선의 스타일을 점선으로 설정
+          });
+
+        } else {
+          linesRef.current.setMap(null); // 선을 지우는 부분
+        }
+
       })
       .catch((error) => {
         console.error("There was an error!", error);
